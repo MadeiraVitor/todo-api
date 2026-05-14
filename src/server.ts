a97f25199f157b05fbbbf1a2b63b6e3356d4bed3
Vitor Madeira <vitorsoutom@hotmail.com>
2026-05-14 10:13:43 -0300

    Creates the registration route for a todo

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
@@ -10,18 +10,33 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
-app.post('/todo', (req, res) => {
-   res.send('cadastrar um novo Todo');
+app.use(express.json());
+
+app.post("/todo", async (req, res) => {
+  const { title, done } = req.body;
+
+  try {
+    await prisma.todo.create({
+      data: {
+        title,
+        done,
+      },
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Erro ao criar o Todo" });
+  }
+
+  res.status(201).send("Todo criado com sucesso");
 });
 
-app.get('/todo', (req, res) => {
-   res.send('buscar todos os Todos cadastrados');
+app.get("/todo", (req, res) => {
+  res.send("buscar todos os Todos cadastrados");
 });
 
-app.delete('/todo/:id', (req, res) => {
-   res.send('deletar um Todo específico');
+app.delete("/todo/:id", (req, res) => {
+  res.send("deletar um Todo específico");
 });
 
 app.listen(port, () => {
-   console.log(`Servidor em execução em http://localhost:${port}`);
+  console.log(`Servidor em execução em http://localhost:${port}`);
 });
